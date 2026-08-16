ebfbc2e9c31e086ae9174186ec7b2c272680edd6
uzma-a <uzmaaasia444@gmail.com>
2026-08-16 15:43:04 -0400

    fixed lint errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -10,14 +9,14 @@ from .populate import initiate
 from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments, post_review
 
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-
 # Create a `login_request` view to handle sign in request
+
+
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
@@ -27,18 +26,16 @@ def login_user(request):
 
     # Try to check if provided credentials can be authenticated
     user = authenticate(username=username, password=password)
-
     data = {"userName": username}
-
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
-
     return JsonResponse(data)
 
-
 # Create a `logout_request` view to handle sign out request
+
+
 def logout_request(request):
     logout(request)
     data = {"userName": ""}
@@ -49,8 +46,6 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-
-
     # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
@@ -59,7 +54,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    
+
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -98,9 +93,10 @@ def get_cars(request):
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
-
 # Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
+
+
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
@@ -109,8 +105,9 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
-
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
+
+
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if (dealer_id):
@@ -124,8 +121,9 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
-
 # Create a `get_dealer_details` view to render the dealer details
+
+
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,15 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 'u25145020210-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'localhost',
+    'u25145020210-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://u25145020210-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://u25145020210-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,10 +102,11 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 
+        'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
