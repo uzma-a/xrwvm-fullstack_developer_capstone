52ba99f84807dc2369a0d5511c0e8b534aaab1b3
uzma-a <uzmaaasia444@gmail.com>
2026-08-16 15:23:18 -0400

    fixed lint errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -49,7 +49,7 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-    context = {}
+
 
     # Load JSON data from the request body
     data = json.loads(request.body)
@@ -59,7 +59,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -136,10 +136,10 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except BaseException:
             return JsonResponse(
