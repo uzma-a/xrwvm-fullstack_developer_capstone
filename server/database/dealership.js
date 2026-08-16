9f55fd8b36112dde664989585a860ca386011cef
uzma-a <uzmaaasia444@gmail.com>
2026-08-16 15:57:48 -0400

    fix lint errors

--- server/database/dealership.js
+++ server/database/dealership.js
@@ -1,13 +1,14 @@
+/*jshint esversion: 8 */
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const dealerships = new Schema({
-	id: {
+  id: {
     type: Number,
     required: true,
-	},
-	city: {
+  },
+  city: {
     type: String,
     required: true
   },
--- server/database/review.js
+++ server/database/review.js
@@ -1,13 +1,14 @@
+/*jshint esversion: 8 */
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const reviews = new Schema({
-	id: {
+  id: {
     type: Number,
     required: true,
-	},
-	name: {
+  },
+  name: {
     type: String,
     required: true
   },
